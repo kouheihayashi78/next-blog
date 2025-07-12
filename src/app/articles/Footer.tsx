cec2c35260b4e5e3579f759588a21022d1d8bfe9
kouhei <kouhei.basketball7.8@gmail.com>
2025-07-13 06:53:04 +0900

    add - ざっくりfooter作成

---
+++ src/app/articles/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-2 mt-5 text-center text-slate-50 border-t'>
+        <small>@2025 k.h</small>
+    </footer>
+  )
+}
+
+export default Footer
