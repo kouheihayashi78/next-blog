cb87817243050a567ba810053ea6f24b2c4ee455
kouhei <kouhei.basketball7.8@gmail.com>
2025-07-15 06:32:50 +0900

    add - ssrでデータを取得し表示する実装

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,46 +1,55 @@
+import { Article } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const ArticleList = () => {
+type ArticleList = {
+  articles: Article[];
+};
+
+const ArticleList = ({ articles }: ArticleList) => {
   return (
     <div>
-      <article>
-        <Link href="#">
-          <Image
-            alt=""
-            src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
-            width={1280}
-            height={300}
-          />
-        </Link>
-      </article>
-      <div className="bg-white flex flex-col justify-start p-6">
-        <Link
-          href="#"
-          className="text-blue-700 text-sm font-bold uppercase pb-4"
-        >
-          Technology
-        </Link>
-        <Link href="#" className="text-slate-800 text-3xl font-bold hover:text-gray-700 pb-4">
-          Lorem Ipsum Dolor Sit Amet Dolor Sit Amet
-        </Link>
-        <p className="text-sm pb-3">
-          By{" "}
-          <Link href="#" className="text-slate-800 font-semibold hover:text-gray-800">
-            David Grzyb
+      {articles.map((article) => (
+        <article className="shadow my-4 flex flex-col" key={article.id}>
+          <Link href={`articles/${article.id}`} className="hover:opacity-75">
+            {/* <Image
+                alt=""
+                src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+                width={1280}
+                height={300}
+            /> */}
           </Link>
-          , Published on April 25th, 2020
-        </p>
-        <Link href="#" className="text-slate-800 pb-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis
-          porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis
-          dui porta volutpat. In sit amet posuere magna..
-        </Link>
-        <Link href="#" className="uppercase text-slate-800 hover:text-blue">
-          Continue Reading <i className="fas fa-arrow-right"></i>
-        </Link>
-      </div>
+
+          <div className="bg-white flex flex-col justify-start p-6">
+            <Link
+              href={`articles/${article.id}`}
+              className="text-blue-700 text-sm font-bold uppercase pb-4"
+            >
+              Technology
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-800 text-3xl font-bold hover:text-gray-700 pb-4"
+            >
+              {article.title}
+            </Link>
+            <p className="text-sm pb-3">Published on {article.createdAt}</p>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-800 pb-6"
+            >
+              {article.content}
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="uppercase text-pink-800 hover:text-black"
+            >
+              続きを読む <i className="fas fa-arrow-right"></i>
+            </Link>
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
 import ArticleList from "./components/ArticleList";
-export default function Home() {
+import { getAllArticles } from "@/blogApi";
+
+export default async function Home() {
+  const articles = await getAllArticles();
+
+  // csrで読み込むならuseEffectを使い、関数のasyncを外し、上で"use client"宣言をする
+  // useEffect(() => {
+  //   const getAllBlogs = async () => {
+  //     const res = await fetch('http://localhost:3001/posts');
+  //     const articles = await res.json();
+  //   }
+  //   getAllBlogs();
+  // }, []);
   return (
     <div className="md:flex">
       <section className="w-full md:w-2/3 flex flex-col items-center px-3">
-        <ArticleList />
+        <ArticleList articles={articles} />
       </section>
       
       <aside className="w-full md:w-1/3 flex flex-col items-center px-3 md:pl-6">
