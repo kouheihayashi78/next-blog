d2e18db24a76dd7e22eaa41e2d709fb474cd2623
kouhei <kouhei.basketball7.8@gmail.com>
2025-07-19 15:05:38 +0900

    add - 子コンポーネントが読み込まれるまでのローディングアニメーションを実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./articles/Header";
 import Footer from "./articles/Footer";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 export const metadata: Metadata = {
   title: "nextBlog",
@@ -17,7 +19,10 @@ export default function RootLayout({
     <html lang="ja">
       <body className='bg-slate-600 text-slate-50 w-full md:w-4/5 m-auto'>
         <Header />
-        {children}
+        {/* Suspenseでラップして子コンポーネントを読みこむまでローディング画面を表示する */}
+        <Suspense fallback={<Loading/>}>
+          {children}
+        </Suspense>
         <Footer />
       </body>
     </html>
