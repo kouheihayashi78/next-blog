76890d3c7d6688dfcd938521fa88068abf809a6d
kouhei <kouhei.basketball7.8@gmail.com>
2025-08-02 17:05:38 +0900

    add - 削除api実装

--- src/pages/api/[id].ts
+++ src/pages/api/[id].ts
@@ -7,18 +7,31 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { id } = req.query;
-  const { data, error } = await supabase
-    .from("posts")
-    .select("*")
-    .eq("id", id)
-    .single();
-  if (error) {
-    return res.status(500).json({ error: error.message });
-  }
 
-  if(!data) {
-    notFound();
-  }
+  switch (req.method) {
+    case "GET":
+      const { data, error } = await supabase
+        .from("posts")
+        .select("*")
+        .eq("id", id)
+        .single();
+      if (error) {
+        return res.status(500).json({ error: error.message });
+      }
 
-  return res.status(200).json(data);
+      if (!data) {
+        notFound();
+      }
+
+      return res.status(200).json(data);
+
+    case "DELETE":
+      const { error: deleteError } = await supabase.from("posts").delete().eq("id", id);
+
+      if (deleteError) {
+        return res.status(500).json({ error: deleteError.message });
+      }
+
+      return res.status(200).json({message: '削除は成功しました。'});
+  }
 }
